71b6a9ae91749f5cfdb145c83175ce6977ce9b9f
MayaLasheen <137602736+MayaLasheen@users.noreply.github.com>
2023-07-29 15:23:09 +0300

    Update testbench.sv

--- testbench.sv
+++ testbench.sv
@@ -4,17 +4,15 @@
 
 module testbench();
   
-  intf i_intf1();
+  intf i_intf();
   
-  test t1(i_intf1);
+  test t(i_intf);
   
-  ALU dut1 (.A(i_intf1.A),
-           .B(i_intf1.B),
-           .ALU_Sel(i_intf1.ALU_Sel),
-           .ALU_Out(i_intf1.ALU_Out),
-           .CarryOut(i_intf1.CarryOut));
-
-
+  ALU dut (.A(i_intf.A),
+           .B(i_intf.B),
+           .ALU_Sel(i_intf.ALU_Sel),
+           .ALU_Out(i_intf.ALU_Out),
+           .CarryOut(i_intf.CarryOut));
   
 endmodule
 
