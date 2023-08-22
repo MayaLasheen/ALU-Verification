823d0dd8f2056d15bfc3bf06b9b746d8796f44bc
MayaLasheen <137602736+MayaLasheen@users.noreply.github.com>
2023-08-22 21:34:59 +0300

    Update Environment.sv

--- Environment.sv
+++ Environment.sv
@@ -1,8 +1,8 @@
-//`include "Transaction.sv"
 `include "Generator.sv"
 `include "Driver.sv"
 `include "Monitor.sv"
 `include "Scoreboard.sv"
+`include "Assertions.sv"
 
 class environment;
   
@@ -10,21 +10,22 @@ class environment;
   driver driv;
   monitor mon;
   scoreboard scb;
+  assertions asrt;
   mailbox #(transaction) gen2driv;
   mailbox #(transaction) mon2scb;
+  mailbox #(transaction) mon2asrt;
   virtual intf vif;
-  //functional_coverage f;
-  //event ok;
   
   function new(virtual intf vif);
     this.vif = vif;
     gen2driv = new();
     mon2scb = new();
+    mon2asrt = new();
     gen = new(gen2driv);
     driv = new(vif, gen2driv);
-    mon = new(vif, mon2scb);
+    mon = new(vif, mon2scb, mon2asrt);
     scb = new(mon2scb);
-    //f = new(vif);
+    asrt = new(mon2asrt);
   endfunction
   
   task test();
@@ -33,15 +34,11 @@ class environment;
       driv.main();
       mon.main();
       scb.main();
-      //scb.displayCoverage();
-      //f.run();
+      asrt.main();
     join
   endtask: test
   
   task run;
-    //scb.ok = ok;
-    //f.ok = ok;
-    
     test();
     $finish;
   endtask: run
